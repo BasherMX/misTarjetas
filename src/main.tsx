af6e42a4884bf302630d18eb3592146329429032
DESKTOP-7J9QCQT\Ulises <brayanvazq1608@gmail.com>
2026-08-28 19:27:24 -0600

    chore: update application runtime setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log(
+  `%c──────────▄\n────────▄██\n─▄▀██▀█▀█▀███▀\n▀▀▀▀▀████▀▀▀\n──────▀██`,
+  'color: #2563eb; font-weight: bold; font-size: 14px;'
+);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
